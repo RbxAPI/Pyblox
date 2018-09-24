a2153e145857bce4c6ad99f169a349b216faf31c
Sanjay <Sanjay-B@users.noreply.github.com>
2018-09-23 22:39:40 -0400

    [User] Better naming structure
    - getUser() is now User()
    - getUserBodyColors() is now BodyColors()
    - getAssetsWorn() is now AssetsWorn()
    - getAssetVersions() is now AssetVersions()
    - getUserPlaces() is now Places()
    - getUserBadges() is now Badges()

--- pyblox/api/user.py
+++ pyblox/api/user.py
@@ -10,7 +10,7 @@ from .http import Http
 import json
 
 
-class User:
+class Users:
 
     # GET https://www.roblox.com/UserCheck/DoesUsernameExist?username={username}
     # Returns Boolean
@@ -25,7 +25,7 @@ class User:
 
     # GET https://api.roblox.com/users/get-by-username?username={username}
     # Returns Table/Array + Attributes
-    def getUser(username):
+    def User(username):
         a = Http.sendRequest("https://api.roblox.com/users/get-by-username?username=" + str(username))
         b = a.decode("utf-8")
         c = json.loads(b)
@@ -40,32 +40,32 @@ class User:
 
     # GET https://www.roblox.com/Asset/BodyColors.ashx?userId={userId}
     # Returns Table/Array
-    def getUserBodyColors(id):
+    def BodyColors(id):
         a = Http.sendRequest("https://www.roblox.com/Asset/BodyColors.ashx?userId=" + str(id))
         return a
 
     # GET https://www.roblox.com/Asset/AvatarAccoutrements.ashx?userId={userId}
     # Returns Table/Array
-    def getAssetsWorn(id):
+    def AssetsWorn(id):
         a = Http.sendRequest("https://www.roblox.com/Asset/AvatarAccoutrements.ashx?userId=" + str(id))
         return a
 
     # GET https://www.roblox.com/Asset/CharacterFetch.ashx?userId={userId}&placeId={placeId}
     # Returns Table/Array
-    def getAssetVersions(id, placeid):
+    def AssetVersions(id, placeid):
         a = Http.sendRequest(
             "https://www.roblox.com/Asset/CharacterFetch.ashx?userId=" + str(id) + "&placeId=" + str(placeid))
         return a
 
     # GET https://www.roblox.com/Contests/Handlers/Showcases.ashx?userId={userId}
     # Returns Table/Array
-    def getUserPlaces(id):
+    def Places(id):
         a = Http.sendRequest("https://www.roblox.com/Contests/Handlers/Showcases.ashx?userId=" + str(id))
         return a
 
     # GET https://www.roblox.com/badges/roblox?userId={userId}&imgWidth=110&imgHeight=110&imgFormat=png
     # Return Table/Array
-    def getUserBadges(id):
+    def Badges(id):
         a = Http.sendRequest(
             "https://www.roblox.com/badges/roblox?userId=" + str(id) + "&imgWidth=110&imgHeight=110&imgFormat=png")
         return a
