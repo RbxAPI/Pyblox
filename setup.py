2dcc0685521d7079a861abcc39540d195aa94c50
Sanjay <Sanjay-B@users.noreply.github.com>
2019-12-03 00:46:01 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+setup(
+  name = 'pyblox3',
+  packages = ['pyblox', 'robloxapi.utils', 'robloxapi.classes'],
+  version = '2.4.1',
+  license='MIT',       
+  description = 'An API wrapper for Roblox written in Python.',
+  long_description = '''
+  An API wrapper for Roblox written in Python.
+  Light-weight version that's aimed to be stable while providing basic functionality.
+  Archived version. 
+  ''',
+  url = 'https://github.com/RbxAPI/Pyblox/tree/master',
+  author = 'Sanjay Bhadra (Sanjay-B)',                
+  author_email = 'n/a',
+  keywords = ['api', 'roblox', 'roblox-api', 'python-api', 'pyblox', 'pyblox3'],
+  install_requires=['requests','beautifulsoup4==4.6.3','html5lib'], # Replace with requirements.txt
+  classifiers=[
+    'Development Status :: 3',      
+    'Intended Audience :: Developers',     
+    'Topic :: Software Development :: Build Tools',
+    'License :: MIT License',  
+    'Programming Language :: Python :: 3.7',
+  ],
+)
