b5b8e3b814999c81d63e29db5c62e32eba2066b8
mfd <minigunneryt@gmail.com>
2020-12-10 18:44:58 +0000

    Consider this a stage

--- pyblox3/src/abtesting.py
+++ pyblox3/src/abtesting.py
@@ -7,15 +7,26 @@
 #
 
 from .util import *
-import json
+
 
 class AbTesting_v1:
 
-    def __init__(self,**kwargs):
+    def __init__(self, **kwargs):
         groupid = kwargs.get("groupid", None)
         auth = kwargs.get("auth", None)
         self.groupid = groupid
         self.auth = auth.auth_cookies
-    
-    def __str__(self):
-        return "AbTesting_v1 class doesn't have any endpoints"
+
+    # POST https://abtesting.roblox.com/v1/get-enrollments
+    async def getEnrollments(self,
+                             **kwargs):
+        auth = self.auth
+        request = kwargs.get('request')
+        response = await Req.request(t='POST',
+                                     url='https://abtesting.roblox.com/v1/get-enrollments',
+                                     auth=auth,
+                                     payload=request)
+        if response[0] == 200:
+            return response[4]
+        else:
+            raise RuntimeError(response[4]['message'])
