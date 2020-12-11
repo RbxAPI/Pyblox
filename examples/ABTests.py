65b7a9a8c9c9360baa3c79dfe6b9c18c8cf0a77f
mfd <minigunneryt@gmail.com>
2020-12-11 16:52:24 +0000

    Add a Copyright notice for MFD, remove import on createGetEnrollmentType, update user, add browserTrackerABTests, add userABTests, implement browserTrackerABTests and userABTests

--- examples/ABTests.py
+++ examples/ABTests.py
@@ -1,28 +1,42 @@
 #
-#  ABTests.py
-#  pyblox
+# ABTests.py
+# pyblox
 #
-#  By nsg-mfd(Nikita Pedko)
-#  Copyright © 2020- Sanjay-B(Sanjay Bhadra) with MFD 2015-2020. All rights reserved.
+# By NSG (Nikita Petko) and IvanG (Ivan Gregrovnich)
+# Copyright © 2015-2020 MFD. All rights reserved.
 #
 
-from pyblox3 import AbTesting_v1, Auth_v2, createGetEnrollmentType
+from pyblox3 import AbTesting_v1, Auth_v2
 import json
 import asyncio
 
-user = Auth_v2(cookies={".ROBLOSECURITY": "Full Cookie found in F12->Application->Left Menu Cookies tab->click the "
-                                          "http://www.roblox.com->.ROBLOSECURITY row where it says, \"Value\" "})
+user = Auth_v2(cookies={".ROBLOSECURITY": "Token"})
 abtestapi = AbTesting_v1(auth=user)
 
-abtests = [
-    createGetEnrollmentType("SomeABTestExperiment", 2, 1345),  # 2 represents BrowserTracker
-    createGetEnrollmentType("SomeABTestExperiment", "BrowserTracker", 1345)
+# The SubjectTargetId when using SubjectType "User" is required to match the UserId of the cookie you supplied.
+# And if a SINGLE Id doesn't match your UserId, it will throw a 403
+
+browserTrackerABTests = [
+    abtestapi.createGetEnrollmentType(ExperimentName="SomeABTestExperiment", SubjectType=2, SubjectTargetId=1234),
+    # 2 represents BrowserTracker and is the same as below:
+    abtestapi.createGetEnrollmentType(ExperimentName="SomeABTestExperiment",
+                                      SubjectType="BrowserTracker",
+                                      SubjectTargetId=1234),
+]
+userABTests = [
+    abtestapi.createGetEnrollmentType(ExperimentName="SomeUserABTestExperiment", SubjectType=1, SubjectTargetId=1234),
+    # 1 represents User and is the same as below:
+    abtestapi.createGetEnrollmentType(ExperimentName="SomeABTestExperiment",
+                                      SubjectType="User",
+                                      SubjectTargetId=1234),
 ]
 
 
 async def event_loop():
-    response = await abtestapi.getEnrollments(request=json.dumps(abtests))
-    print(response)
+    browserTrackerResponse = await abtestapi.getEnrollments(request=json.dumps(browserTrackerABTests))
+    userResponse = await abtestapi.getEnrollments(request=json.dumps(userABTests))
+    print(browserTrackerResponse, userResponse)
+
 
 loop = asyncio.get_event_loop()  # Get the event loop
 loop.run_until_complete(event_loop())  # Bind the your event loop to the main one
