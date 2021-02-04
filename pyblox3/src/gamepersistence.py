418a30a7e00769de484d7859a034f0ab80cfce79
mfd-co <tfs@mfdlabs.com>
2021-02-04 20:42:29 +0000

    Initial commit to get PR open, don't pull.

---
+++ pyblox3/src/gamepersistence.py
@@ -0,0 +1,10 @@
+#
+# gamepersistence.py
+# pyblox
+#
+# By NSG (Nikita Petko) with Sanjay-B(Sanjay Bhadra)
+# Copyright 2019-2021 Sanjay-B, All rights reserved.
+# Copyright 2011-2021 Nikita Petko, All rights reserved.
+# Copyright 2003-2021 ROBLOX Corporation, All Rights Reserved.
+# Copyright 2002-2021 MFD Corporation, All Rights Reserved.
+#
