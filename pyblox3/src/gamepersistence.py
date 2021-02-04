06f3ec299584345caa4dea2410022aa7a795f01c
mfd-co <tfs@mfdlabs.com>
2021-02-04 22:04:27 +0000

    Add classes for whatever reason, Jack Daniels is contributing also

--- pyblox3/src/gamepersistence.py
+++ pyblox3/src/gamepersistence.py
@@ -8,3 +8,25 @@
 # Copyright 2003-2021 ROBLOX Corporation, All Rights Reserved.
 # Copyright 2002-2021 MFD Corporation, All Rights Reserved.
 #
+
+class GamePersistence_NoOpt:
+    def __init__(self, **kwargs):
+        auth = kwargs.get("auth", None)
+        self.auth = auth.auth_cookies
+
+# Deprecate this soon, v0 endpoints are being removed when DataStoreV2 is released for BetaTesters, currently only available for SoothSayer anyway
+# - Jack Puickeikzi Daniels (Core Game Engine Systems Engineer - ROBLOX)
+class GamePersistence_v0:
+    def __init__(self, **kwargs):
+        auth = kwargs.get("auth", None)
+        self.auth = auth.auth_cookies
+
+class GamePersistence_v1:
+    def __init__(self, **kwargs):
+        auth = kwargs.get("auth", None)
+        self.auth = auth.auth_cookies
+
+class GamePersistence_v2:
+    def __init__(self, **kwargs):
+        auth = kwargs.get("auth", None)
+        self.auth = auth.auth_cookies
